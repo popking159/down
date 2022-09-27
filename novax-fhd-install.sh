72da053121252a1e55e559d5bd0950b66418781d
popking159 <47087407+popking159@users.noreply.github.com>
2022-09-27 18:21:55 +0200

    Add files via upload

---
+++ novax-fhd-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+##setup command=wget -q "--no-check-certificate" https://github.com/popking159/down/raw/master/estury_posters.sh -O - | /bin/sh
+
+echo ""
+
+wget -O  /var/volatile/tmp/Novax-FHD-MNASR-v1.0.tar.gz https://github.com/popking159/down/raw/master/Novax-FHD-MNASR-v1.0.tar.gz
+tar -xzf /var/volatile/tmp/Novax-FHD-MNASR-v1.0.tar.gz -C /
+rm -rf /var/volatile/tmp/Novax-FHD-MNASR-v1.0.tar.gz
+
+
+sync
+echo "#########################################################"
+
+echo "#########################################################"
+sleep 3
+#killall enigma2
+exit 0
